ba6ecc4759428e86817339a6261cb009aec0dd9d
lonardo123 <65513096+lonardo123@users.noreply.github.com>
2025-08-22 05:21:42 +0300

    Create init.sql

---
+++ migrations/init.sql
@@ -0,0 +1,5 @@
+-- سيتم تنفيذها تلقائيًا في database.js
+-- تم تضمينها كمرجع
+CREATE TABLE IF NOT EXISTS users (...);
+CREATE TABLE IF NOT EXISTS earnings (...);
+CREATE TABLE IF NOT EXISTS withdrawals (...);
