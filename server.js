f914a1df4040642837f0bfc206453c8cc10c251a
lonardo123 <65513096+lonardo123@users.noreply.github.com>
2025-08-22 05:29:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const callbackRouter = require('./handlers/callback');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use('/callback', callbackRouter);
