0e54eb5774e1709ca42926490a082022c4be533e
lonardo123 <65513096+lonardo123@users.noreply.github.com>
2025-08-22 18:24:52 +0300

    Delete migrations directory

--- migrations/init.sql
+++
@@ -1,5 +0,0 @@
--- سيتم تنفيذها تلقائيًا في database.js
--- تم تضمينها كمرجع
-CREATE TABLE IF NOT EXISTS users (...);
-CREATE TABLE IF NOT EXISTS earnings (...);
-CREATE TABLE IF NOT EXISTS withdrawals (...);
