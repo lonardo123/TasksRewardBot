4e1560072098edec08810c56faa0ecf52ec6ccc6
lonardo123 <65513096+lonardo123@users.noreply.github.com>
2025-08-22 17:12:49 +0300

    Update keyboards.js

--- views/keyboards.js
+++ views/keyboards.js
@@ -2,23 +2,16 @@ const { Markup } = require('telegraf');
 
 const mainMenu = () =>
   Markup.keyboard([
-    ['💰 رصيدك'],
-    ['🎁 مصادر الربح'],
+    ['💰 رصيدك', '🎁 مصادر الربح'],
     ['📤 طلب سحب']
   ]).resize();
-
-const offersMenu = () =>
-  Markup.inlineKeyboard([
-    Markup.button.url('TimeWall', 'https://timewall.example.com/?user_id=' + userId),
-    Markup.button.url('cpalead', 'https://cpalead.example.com/?user_id=' + userId)
-  ]);
 
 const adminMenu = () =>
   Markup.keyboard([
     ['📋 عرض الطلبات'],
-    ['🔧 تعديل الحد الأدنى'],
     ['📊 الإحصائيات'],
+    ['🔧 تعديل الحد الأدنى'],
     ['🚪 خروج من لوحة الأدمن']
   ]).resize();
 
-module.exports = { mainMenu, offersMenu, adminMenu };
+module.exports = { mainMenu, adminMenu };
