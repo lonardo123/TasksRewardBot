363de3a1c98aa4d4ebe2a69eeb33279c713a9b6f
lonardo123 <65513096+lonardo123@users.noreply.github.com>
2025-09-28 12:54:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -72,6 +72,7 @@ async function connectDB() {
 // === السيرفر ===
 const app = express();
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('✅ السيرفر يعمل! Postback جاهز.');
