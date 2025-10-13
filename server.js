5eff658372ed494729bef198aa6e31c6c7012b45
lonardo123 <65513096+lonardo123@users.noreply.github.com>
2025-10-13 03:09:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -22,9 +22,6 @@ app.get('/worker/start', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/worker/start.html'));
 });
 
-app.get('/Main/Backend/Assets/css/worker_start.css?v=1%3A0%3A1', (req, res) => {
-  res.sendFile(path.join(__dirname, '/Main/Backend/Assets/css/worker_start.css?v=1%3A0%3A1'));
-});
 
 // ===========================================
 // ✅ مسار التحقق من العامل (Worker Verification)
